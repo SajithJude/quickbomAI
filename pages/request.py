db20d49203fee1bd6f1f04edec4e435bcd84662c
Jude Sajith <judesajith.aj@gmail.com>
2023-04-16 20:32:58 +0530

    ssss

--- pages/request.py
+++ pages/request.py
@@ -9,20 +9,24 @@ API_URL = 'https://beta.api.oemsecrets.com/partsearch?searchTerm={}&apiKey={}'
 API_KEY = os.getenv('API_KEY')
 
 # Define a list of items to search for
-items = ['TWO GANG SWITCH', 'single switch', 'three gang switch']
+items = ['2 gang switch', 'single switch', 'three gang switch']
+
 
 # Define a function to send a request to the API and display the response
 def search_item(item):
     url = API_URL.format(item.replace(' ', '%20'), API_KEY)
     response = requests.get(url)
     data = response.json()
-    st.write(data)
+    for stock_item in data['stock']:
+        for currency, prices in stock_item['prices'].items():
+            for price in prices:
+                if price['unit_break'] == '1':
+                    unit_price = price['unit_price']
+                    st.write(f"The unit price for {item} is {unit_price} {currency}.")
 
 # Create a Streamlit app with a loop that displays a button for each item in the list
 st.title('Part Search App')
-if st.button('Search'):
-    for item in items:
-        st.write(f'Searching for {item}...')
-        
+for item in items:
+    st.write(f'Searching for {item}...')
+    if st.button('Search'):
         search_item(item)
-    st.stop()
