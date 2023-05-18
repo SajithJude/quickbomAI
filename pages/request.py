1ddcd95ab409064f2f867ff066f9c260cb76b2c0
Jude Sajith <judesajith.aj@gmail.com>
2023-05-18 22:59:37 +0530

    ssss

--- pages/request.py
+++ pages/request.py
@@ -27,30 +27,34 @@ headers = {
 if st.button("make request"):
     response = requests.get('https://api.element14.com/catalog/products', headers=headers, params=params)
 
-# Check the response status
-if response.status_code == 200:
-    # Load the data into a dataframe
-    data = response.json()
-    products = data['keywordSearchReturn']['products']
 
-    # Display each product
-    for product in products:
-        st.subheader(product['displayName'])
-        st.write(f'SKU: {product["sku"]}')
-        st.write(f'Product Status: {product["productStatus"]}')
-        st.write(f'ROHS Status Code: {product["rohsStatusCode"]}')
-        st.write(f'Pack Size: {product["packSize"]}')
-        st.write(f'Unit of Measure: {product["unitOfMeasure"]}')
-        st.image(f'https://uk.farnell.com{product["image"]["baseName"]}')
+try:
+    # Check the response status
+    if response.status_code == 200:
+        # Load the data into a dataframe
+        data = response.json()
+        products = data['keywordSearchReturn']['products']
 
-        st.subheader('Datasheets:')
-        for datasheet in product['datasheets']:
-            st.write(f'Description: {datasheet["description"]}')
-            st.write(f'URL: {datasheet["url"]}')
+        # Display each product
+        for product in products:
+            st.subheader(product['displayName'])
+            st.write(f'SKU: {product["sku"]}')
+            st.write(f'Product Status: {product["productStatus"]}')
+            st.write(f'ROHS Status Code: {product["rohsStatusCode"]}')
+            st.write(f'Pack Size: {product["packSize"]}')
+            st.write(f'Unit of Measure: {product["unitOfMeasure"]}')
+            st.image(f'https://uk.farnell.com{product["image"]["baseName"]}')
 
-        st.subheader('Prices:')
-        for price in product['prices']:
-            st.write(f'From: {price["from"]}, To: {price["to"]}, Cost: {price["cost"]}')
+            st.subheader('Datasheets:')
+            for datasheet in product['datasheets']:
+                st.write(f'Description: {datasheet["description"]}')
+                st.write(f'URL: {datasheet["url"]}')
 
-else:
-    st.error(f'Request failed with status code {response.status_code}')
+            st.subheader('Prices:')
+            for price in product['prices']:
+                st.write(f'From: {price["from"]}, To: {price["to"]}, Cost: {price["cost"]}')
+
+    else:
+        st.error(f'Request failed with status code {response.status_code}')
+except (NameError) as e:
+    st.info("Input search keyword and click make request")
